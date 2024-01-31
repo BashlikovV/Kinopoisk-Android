3c3f3380dcb1d4ea0cbe1c32d70175ee66ef0f3f
bashlikovvv <volodia413314@gmail.com>
2024-01-31 11:42:20 +0300

    Added calculation of the size of the menu item depending on the size of the screen.

--- app/src/main/java/by/bashlikovvv/kinopoisk_android/presentation/view/CustomBottomNavigationView.kt
+++ app/src/main/java/by/bashlikovvv/kinopoisk_android/presentation/view/CustomBottomNavigationView.kt
@@ -1,20 +1,25 @@
 package by.bashlikovvv.kinopoisk_android.presentation.view
 
 import android.animation.ValueAnimator
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Rect
 import android.graphics.RectF
+import android.os.Build
 import android.util.AttributeSet
-import android.util.Log
-import android.view.MenuItem
+import android.util.DisplayMetrics
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.view.doOnPreDraw
+import androidx.core.view.forEach
 import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
 import by.bashlikovvv.kinopoisk_android.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.math.abs
+
 
 private const val DEFAULT_SCALE = 1f
 private const val MAX_SCALE = 15f
@@ -27,12 +32,32 @@ class CustomBottomNavigationView : BottomNavigationView {
     private var animator: ValueAnimator? = null
 
     private val indicator = RectF()
+//    private val rects = Array(size = 4) { RectF() }
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = ContextCompat.getColor(context, R.color.white)
+        color = ContextCompat.getColor(context, R.color.indicator_selected)
     }
 
-    private val bottomOffset = 5f
+    private val bottomOffset = 15f
     private var defaultSize = 5f
+    private val diff: Int by lazy {
+        var lastViewRight = 0
+        menu.forEach {
+            lastViewRight = maxOf(findViewById<View>(it.itemId).right, lastViewRight)
+        }
+        val activity = getActivity()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val bouds = activity?.windowManager?.currentWindowMetrics?.bounds ?: Rect()
+
+            bouds.right - bouds.left - lastViewRight
+        } else {
+            val display = activity?.windowManager?.defaultDisplay
+            val displayMetrics = DisplayMetrics()
+            display?.getMetrics(displayMetrics)
+
+            displayMetrics.widthPixels - lastViewRight
+        }
+    }
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
@@ -40,6 +65,7 @@ class CustomBottomNavigationView : BottomNavigationView {
             : super(context, attrs, defStyleAttr)
 
     init {
+        itemActiveIndicatorShapeAppearance = null
         super.setOnItemSelectedListener {
             if (externalSelectedListener?.onNavigationItemSelected(it) != false) {
                 onItemSelected(it.itemId, true)
@@ -72,6 +98,9 @@ class CustomBottomNavigationView : BottomNavigationView {
         if (isLaidOut) {
             val cornerRadius = indicator.height() / 2f
             canvas.drawRoundRect(indicator, cornerRadius, cornerRadius, paint)
+//            rects.forEach { rect ->
+//                canvas.drawRect(rect, paint)
+//            }
         }
     }
 
@@ -80,19 +109,51 @@ class CustomBottomNavigationView : BottomNavigationView {
 
         cancelAnimator(setEndValues = false)
 
+//        (itemView.right - itemView.left).toFloat() / 2
         val itemView = findViewById<View>(itemId) ?: return
-        defaultSize = (itemView.left - itemView.right).toFloat() / 2
+        defaultSize = if (itemView.left == 0) {
+//            rects[0].set(
+//                itemView.left.toFloat(),
+//                itemView.top.toFloat(),
+//                (itemView.left + 5).toFloat(),
+//                itemView.bottom.toFloat()
+//            )
+//            rects[1].set(
+//                itemView.right.toFloat() + diff / 2,
+//                itemView.top.toFloat(),
+//                (itemView.right - 5).toFloat() + diff / 2,
+//                itemView.bottom.toFloat()
+//            )
+
+            (itemView.right.toFloat() + diff / 2) - itemView.left
+        } else {
+//            rects[2].set(
+//                itemView.left.toFloat() + diff / 2,
+//                itemView.top.toFloat(),
+//                (itemView.left + 5).toFloat() + diff / 2,
+//                itemView.bottom.toFloat()
+//            )
+//            rects[3].set(
+//                itemView.right.toFloat() + diff,
+//                itemView.top.toFloat(),
+//                (itemView.right - 5).toFloat() + diff,
+//                itemView.bottom.toFloat()
+//            )
+
+            (itemView.right + diff) - (itemView.left.toFloat() + diff / 2)
+        }
+
         val fromCenterX = indicator.centerX()
         val fromScale = indicator.width() / defaultSize
 
         animator = ValueAnimator.ofFloat(fromScale, MAX_SCALE, DEFAULT_SCALE).apply {
             addUpdateListener {
                 val progress = it.animatedFraction
-                val distanceTravelled = linearInterpolation(progress, fromCenterX, itemView.centerX)
+                val distanceTravelled = linearInterpolation(progress, fromCenterX, itemView.centerX + diff / 2)
 
-                val left = distanceTravelled - defaultSize / 2
+                val left = distanceTravelled - defaultSize / 4
                 val top = height - bottomOffset - 10
-                val right = distanceTravelled + defaultSize / 2
+                val right = distanceTravelled + defaultSize / 4
                 val bottom = height - bottomOffset
 
                 indicator.set(
@@ -128,5 +189,16 @@ class CustomBottomNavigationView : BottomNavigationView {
         }
         it.removeAllUpdateListeners()
         animator = null
+    }
+
+    private fun getActivity(): Activity? {
+        var context = context
+        while (context is ContextWrapper) {
+            if (context is Activity) {
+                return context
+            }
+            context = context.baseContext
+        }
+        return null
     }
 }
