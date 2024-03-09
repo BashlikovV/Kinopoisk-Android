ee644586a304112f74d2a2232a2f8be77298e6d4
bashlikovvv <volodia413314@gmail.com>
2024-03-09 17:41:30 +0300

    Code cleaned.

--- core/src/main/java/by/bashlikovvv/core/ext/FragmentExt.kt
+++ core/src/main/java/by/bashlikovvv/core/ext/FragmentExt.kt
@@ -1,78 +1,13 @@
+@file:Suppress("UNUSED")
 package by.bashlikovvv.core.ext
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlin.coroutines.EmptyCoroutineContext
-
-inline fun Fragment.launchIO(
-    crossinline safeAction: suspend () -> Unit,
-    crossinline onError: (Throwable) -> Unit,
-    errorDispatcher: CoroutineDispatcher = Dispatchers.Main
-): Job {
-    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        lifecycleScope.launch(errorDispatcher) {
-            onError.invoke(throwable)
-        }
-    }
-
-    return lifecycleScope.launch(exceptionHandler + Dispatchers.IO) {
-        safeAction.invoke()
-    }
-}
-
-inline fun Fragment.launchMain(
-    crossinline safeAction: suspend () -> Unit,
-    crossinline onError: (Throwable) -> Unit
-): Job {
-    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        lifecycleScope.launch(Dispatchers.Main) {
-            onError.invoke(throwable)
-        }
-    }
-
-    return lifecycleScope.launch(exceptionHandler + Dispatchers.Main) {
-        safeAction.invoke()
-    }
-}
-
-inline fun Fragment.launchDefault(
-    crossinline safeAction: suspend () -> Unit,
-    crossinline onError: (Throwable) -> Unit,
-    errorDispatcher: CoroutineDispatcher = Dispatchers.Main
-): Job {
-    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        lifecycleScope.launch(errorDispatcher) {
-            onError.invoke(throwable)
-        }
-    }
-
-    return lifecycleScope.launch(exceptionHandler + Dispatchers.Default) {
-        safeAction.invoke()
-    }
-}
-
-inline fun Fragment.launchUnconfined(
-    crossinline safeAction: suspend () -> Unit,
-    crossinline onError: (Throwable) -> Unit,
-    errorDispatcher: CoroutineDispatcher = Dispatchers.Main
-): Job {
-    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        lifecycleScope.launch(errorDispatcher) {
-            onError.invoke(throwable)
-        }
-    }
-
-    return lifecycleScope.launch(exceptionHandler + Dispatchers.Unconfined) {
-        safeAction.invoke()
-    }
-}
-
-/*  */
 
 inline fun Fragment.launchIO(
     crossinline safeAction: suspend () -> Unit,
