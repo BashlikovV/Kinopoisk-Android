361658a2341d05ffb2f632f15828ee8b00c6c105
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2024-04-15 20:27:58 +0300

    Update Constants.kt

--- core/src/main/java/by/bashlikovvv/core/Constants.kt
+++ core/src/main/java/by/bashlikovvv/core/Constants.kt
@@ -2,7 +2,7 @@ package by.bashlikovvv.core
 
 object Constants {
 
-    const val API_KEY = "FR4GD5N-DH24DJM-QM6P7VV-06PXA3S"
+    const val API_KEY = ""
     const val BASE_URL = "https://api.kinopoisk.dev/v1.4/"
     const val API_KEY_NAME = "X-API-KEY"
     const val PAGE_SIZE = 10
